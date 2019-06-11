3be0bb27b624ae42f02795e6c5cee2120ade1505
oneLuckyStar <51134043+oneLuckyStar@users.noreply.github.com>
2019-06-11 09:29:55 +0300

    Add files via upload

---
+++ lab3.hs
@@ -0,0 +1,9 @@
+﻿reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+main = do {
+putStrLn "Введите строку длиной 5:";
+a<-getLine;
+putStrLn ("реверсирование = " ++ (reverse'(read a :: [Char])) ++ (reverse'(init(read a :: String))) ++( reverse' (take 3(read a :: String)))++( reverse'(take 2(read a :: String)))++( reverse' (take 1(read a :: String))));
+}
+
